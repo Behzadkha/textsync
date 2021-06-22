89cbb37dec06bce886122be843dcf3012fb5a115
Behzad Khamneli <bkhamneli6@gmail.com>
2021-06-22 14:48:03 -0400

    Sender/Receiver added to the homepage

--- Frontend/textsync/src/components/Home.js
+++ Frontend/textsync/src/components/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../componentsStyle/Home.css'
+import { Button } from 'react-bootstrap';
+import Sender from '../components/Sender';
+import Receiver from '../components/Receiver';
+
 export default function Home() {
+    const [SR, setSR] = useState("Both")
+    function checkSR() {
+        if (SR === "Both") {
+            return (
+                <div id="HomeStyle">
+                    <h1 id="HomeIAMA">I'm a</h1>
+                    <Button id="HomeS" onClick={() => setSR("S")}>Sender</Button>
+                    <Button id="HomeR" onClick={() => setSR("R")}>Receiver</Button>
+                </div>
+            )
+        }
+        else if (SR === "S") { return (<Sender />) }
+        else { return (<Receiver />) }
+    }
     return (
         <div>
-            <h1 className="HomeTitle">Share text between your devices easily!</h1>
+            <div>
+                <h1 className="HomeTitle">Share text between your devices easily!</h1>
+            </div>
+            <div id="HomeStyle">
+                <h1 id="HomeIAMA">I'm a</h1>
+                <Button id="HomeS">Sender</Button>
+                <Button id="HomeR">Receiver</Button>
+            </div>
+
         </div>
     )
 }
