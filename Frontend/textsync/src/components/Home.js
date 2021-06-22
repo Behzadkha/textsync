a49f71c95cc709485d65e4b479c5a6768dc3c6c7
Behzad Khamneli <bkhamneli6@gmail.com>
2021-06-22 14:36:20 -0400

    Working on homepage

---
+++ Frontend/textsync/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../componentsStyle/Home.css'
+export default function Home() {
+    return (
+        <div>
+            <h1 className="HomeTitle">Share text between your devices easily!</h1>
+        </div>
+    )
+}
