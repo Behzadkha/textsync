6b2c9a5197cf501e6d8616534ca2a60185aeb974
Behzad Khamneli <bkhamneli6@gmail.com>
2021-09-17 10:03:02 -0400

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,4 @@
 const config = {
-    database: "mongodb+srv://behzad:TextSyncProject@cluster0.mntyb.mongodb.net/Database?retryWrites=true&w=majority"
+    database: "mongodb+srv://behzad:{p}@cluster0.mntyb.mongodb.net/Database?retryWrites=true&w=majority"
 }
 module.exports = config;
